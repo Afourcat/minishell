013516381f97bb249dfffc5d4afbb41e048e9238
Alexandre FOURCAT <afourcat@localhost.localdomain>
2018-01-16 12:02:59 +0100

    bug that allow user to make double env value with the same name

--- sources/built_in/setenv.c
+++ sources/built_in/setenv.c
@@ -21,17 +21,22 @@ int cmd_len(char *cmd[])
 int my_setenv(char *cmd[], struct env_t *env)
 {
 	int size = cmd_len(cmd);
-
+	
 	if (size <= 2) {
-		if (size == 1)
+		if (size == 1) {
 			my_env(cmd, env);
-		else
+		} else {
+			(env_get_value(env, cmd[1]) != NULL) ?	\
+				env_pop(env, cmd[1]) : 0;
 			env_push_two_part(env, cmd[1], "");
+		}
 		return (0);
 	} else if (size > 4) {
 		write(2, "setenv: Too many arguments.\n", 29);
 		return (1);
 	} else {
+		if (env_get_value(env, cmd[1]) != NULL)
+			env_pop(env, cmd[1]);
 		env_push_two_part(env, cmd[1], cmd[2]);
 		return (0);
 	}
